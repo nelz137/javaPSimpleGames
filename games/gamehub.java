51755c6cc90ba931aa72713f46fc3485c29b2236
Your Name <vasvaneel1983@gmail.com>
2025-09-21 20:17:24 +0530

    all things are changed

---
+++ games/gamehub.java
@@ -0,0 +1,32 @@
+package games;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Scanner;
+
+public class gamehub extends JFrame{
+public gamehub() {
+setTitle("Java Game Hub");
+setSize(400,400);
+
+setDefaultCloseOperation(EXIT_ON_CLOSE);
+setLayout(new GridLayout(5,1,10,10));
+
+JButton btn1 =new JButton("Tic Tack Toe");
+JButton btn2 = new JButton("Snake Game");
+
+
+
+
+
+btn1.addActionListener(e-> new tic().startGame());
+
+add(btn1);
+setVisible(true);
+
+}
+
+public static void main(String[] args) {
+new gamehub();
+}
+}
