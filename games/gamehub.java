343995683766cdbe1f47e5ebbf648d3f8c3c1910
Your Name <vasvaneel1983@gmail.com>
2025-09-22 22:48:41 +0530

    some eye catching Layout

--- games/gamehub.java
+++ games/gamehub.java
@@ -6,25 +6,68 @@ import java.awt.event.*;
 
 public class gamehub extends JFrame{
 public gamehub() {
-setTitle("Java Game Hub");
-setSize(400,400);
+setTitle("Java Simple Games");
+setSize(500,600);
 
 setDefaultCloseOperation(EXIT_ON_CLOSE);
-setLayout(new GridLayout(5,1,10,10));
+setLocationRelativeTo(null);
+//setLayout(new GridLayout(5,1,10,10));
+JPanel panel=new JPanel();
+panel.setLayout(new GridBagLayout());
+panel.setBackground(new Color(30,30,30));
+
+GridBagConstraints gbc =new GridBagConstraints();
+
+gbc.insets=new Insets(15, 0, 15, 0);
+gbc.fill= GridBagConstraints.HORIZONTAL;
+
+JLabel Titlle= new JLabel("Simple Games",SwingConstants.CENTER);
+Titlle.setFont(new Font("Arial",Font.BOLD,32));
+Titlle.setForeground(Color.WHITE);
+gbc.gridx=0;
+gbc.gridy=0;
+panel.add(Titlle,gbc);
+
+
 
 JButton btn1 =new JButton("Tic Tack Toe");
 JButton btn2 = new JButton("Snake Game");
 
 
+gbc.gridy=1;panel.add(btn1,gbc);
 
 
 
 btn1.addActionListener(e-> new tic().startGame());
 
-add(btn1);
+add(panel);
 setVisible(true);
 
 }
+private JButton createMenuButton (String text){
+JButton button =new JButton(text);
+button.setFont(new Font("Arial",Font.BOLD,20));
+button.setForeground(Color.WHITE);
+button.setBackground(new Color(70,130,180));
+button.setFocusPainted(false);
+button.setBorderPainted(false);
+setPreferredSize(new Dimension(250,50));
+button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+button.addMouseListener(new java.awt.event.MouseAdapter() {
+    public void mouseEntered(java.awt.event.MouseEvent evt){
+        button.setBackground(new Color(100,149,237));
+
+    }
+    public void mouseExited(java.awt.event.MouseEvent evt){
+        button.setBackground(new Color(70,130,180));
+    }
+
+});
+return button;
+}
+
+
 
 public static void main(String[] args) {
 new gamehub();
